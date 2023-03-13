f21dd371a179cb33154c0bab291db9ec15d1c33b
Rana Usama <ranausama@Usamas-MacBook-Pro.local>
2023-03-13 19:34:11 +0500

    started

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hamail's FYP</Text>
       <StatusBar style="auto" />
     </View>
   );
